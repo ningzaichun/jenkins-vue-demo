2139cd7729ce5e2eb9367c8fb37cf18a90172a17
Crush <951930136@qq.com>
2022-08-02 00:00:46 +0800

    fix:调整路径

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+module.exports = {
   publicPath: './',
   outputDir: 'dist',
   transpileDependencies: true
-})
+}
